fd521334f61edbe6485d76aa9905523a3264c161
Adrien <47657977+AdrienTheRealProgrammer@users.noreply.github.com>
2020-04-16 16:44:23 +0200

    message

--- Pages/AjoutProduit.php
+++ Pages/AjoutProduit.php
@@ -7,9 +7,7 @@
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
+<head>
         <title>Ajouter un produit</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -17,17 +15,43 @@
         <link rel="stylesheet" type="text/css" href="../CSS/StyleCreation.css">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+        <script type="text/javascript">
+            function blocage1(){
+            if(document.getElementById('inEncheres').checked == true)
+            {
+                document.getElementById('inAchatImmediat').disabled = true;
+                document.getElementById('inBestOffer').disabled = true;
+            }
+            else{
+                document.getElementById('inAchatImmediat').disabled = false;
+                document.getElementById('inBestOffer').disabled = false;
+            }
+            }
+
+            function blocage2(){
+            if(document.getElementById('inAchatImmediat').checked==true||document.getElementById('inBestOffer').checked==true)
+            {
+                document.getElementById('inEncheres').disabled = true;
+            }
+            else
+            {
+                document.getElementById('inEncheres').disabled = false;
+            }
+            }
+        </script>
     </head>
 
     <body>
         <?php include 'navbar.php'; ?>
+        <!DOCTYPE html>
 
         <div class="container features">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12">
                     <h1 class="feature-title">Mise en vente d'un produit</h1>
 
-                    <form method="POST" action="#"> <!-- Manque lien -->
+                    <form method="POST" action="ajoutArticle.php"> 
                         <h4>| Informations produit</h4>
 
                         <!-- Inspiré de https://getbootstrap.com/docs/4.0/components/forms/ -->
@@ -39,49 +63,48 @@
                         <label>Type de produit:&nbsp;&nbsp;&nbsp;</label>
                         <div class="form-group">
                             <div class="custom-control custom-radio custom-control-inline">
-                                    <input class="form-check-input" type="radio" name="typeProd" id="inFerrailleOuTresor">
+                                    <input class="form-check-input" type="radio" name="typeProd" id="inFerrailleOuTresor" value="FerrailleOuTresor" >
                                     <label class="form-check-label" for="inFerrailleOuTresor">Ferraille ou Trésor</label>
                             </div>
                             <div class="custom-control custom-radio custom-control-inline">
-                                    <input class="form-check-input" type="radio" name="typeProd" id="inMusee">
+                                    <input class="form-check-input" type="radio" name="typeProd" id="inMusee" value="Musee">
                                     <label class="form-check-label" for="inMusee">Musée</label>
                             </div>
                             <div class="custom-control custom-radio custom-control-inline">
-                                    <input class="form-check-input" type="radio" name="typeProd" id="inVIP">
+                                    <input class="form-check-input" type="radio" name="typeProd" id="inVIP" value="VIP">
                                     <label class="form-check-label" for="inVIP">Accessoires VIP</label>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inDesc">Description</label>
-                            <textarea class="form-control" name="descripiton" id="inDesc" rows="5">Mon produit est...</textarea>
+                            <textarea class="form-control" name="description" id="inDesc" rows="5">Mon produit est...</textarea>
                         </div>
-
                         <div class="form-group">
                             <label>Type de vente</label>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="typeProd" id="inFerrailleOuTresor">
-                                <label class="form-check-label" for="inFerrailleOuTresor">Ferraille ou Trésor</label>
+                                <input class="form-check-input" type="checkbox" name="VenteEnchere" id="inEncheres" onclick="blocage1()" value=1>
+                                <label class="form-check-label" for="inEncheres">Encheres</label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="typeProd" id="inMusee">
-                                <label class="form-check-label" for="inMusee">Musée</label>
+                                <input class="form-check-input" type="checkbox" name="VenteImmediat" id="inAchatImmediat" onclick="blocage2()" value=1>
+                                <label class="form-check-label" for="inAchatImmediat">Achat Immediat</label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="typeProd" id="inVIP">
-                                <label class="form-check-label" for="inVIP">Accessoires VIP</label>
+                            <input class="form-check-input" type="checkbox" name="VenteBestOffer" id="inBestOffer" onclick="blocage2()" value=1>
+                                <label class="form-check-label" for="inBestOffer">BestOffer</label>
                             </div>      
-                        </div>
+                          </div>
 
                         <div class="form-group">
-                            <label for="inPrix">Prix de départ</label>
-                            <input type="number" class="form-control" id="inPrix" placeholder="Ex: 500€" name="prixDep">
+                            <label for="inPrix">Prix (en €)</label>
+                            <input type="number" class="form-control" id="inPrix" placeholder="Ex: 500" name="prix">
                         </div>
                         <div class="form-group">
                             <label for="inDateLim">Date Limite</label>
                             <input type="date" class="form-control" id="inDateLim" name="dateLim">
                         </div>
 
-                        <label>AJout de photos/video</label>
+                        <label>Ajout de photos/video</label>
                         <div class="form-row">
                             <div class="col">
                                 <label for="inImg1">Image 1</label>
@@ -91,13 +114,18 @@
                                 <label for="inImg2">Image 2</label>
                                 <input type="file" class="form-control-file" name="img2">
                             </div>
-                            <div class="col">
-                                <label for="inVid">Video (optionel)</label>
-                                <input type="file" class="form-control-file" name="video">
-                              </div>
-                        </div>
 
-                        <button type="submit" class="btn btn-primary btn-block">Soumettre</button>
+                            <div class="col">
+                                <label for="inImg3">Image 2</label>
+                                <input type="file" class="form-control-file" name="img3">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="inVid">Lien Youtube vers la vidéo</label>
+                                <input type="text" class="form-control" id="inVid" placeholder="" name="video">
+                            </div>
+
+                        <button type="submit" class="btn btn-primary btn-block" name="button1">Soumettre</button>
                     </form>
                 </div>
             </div>
