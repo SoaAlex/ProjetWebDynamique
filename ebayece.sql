7863f292e75e144b58f867ad5e0142e279cfa9e7
SoaAlex <l.soaresalex@gmail.com>
2020-04-20 09:54:06 +0200

    bdd avec plus d'articles

--- ebayece.sql
+++ ebayece.sql
@@ -2,8 +2,8 @@
 -- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 19 avr. 2020 à 15:01
+-- Hôte : 127.0.0.1:3308
+-- Généré le :  lun. 20 avr. 2020 à 07:40
 -- Version du serveur :  8.0.18
 -- Version de PHP :  7.3.12
 
@@ -125,17 +125,24 @@ CREATE TABLE IF NOT EXISTS `article` (
   KEY `#IDCommande` (`#IDCommande`),
   KEY `#IDVendeur` (`#IDVendeur`),
   KEY `#IDAdmin` (`#IDAdmin`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `article`
 --
 
 INSERT INTO `article` (`IDArticle`, `Nom`, `Description`, `TypeArticle`, `Prix`, `VenteEnchere`, `VenteImmediat`, `VenteBestOffer`, `DateLim`, `#IDCommande`, `#IDVendeur`, `#IDAdmin`, `CheminVideo`) VALUES
-(1, 'Piece rare', 'Je suis une description nulle d\'article. Je suis une description nulle d\'article. Je suis une description nulle d\'article. Je suis une description nulle d\'article.\r\nJe suis une description nulle d\'article.\r\nJe suis une description nulle d\'article.', 'Ferraille', 50, 1, 0, 0, '2020-04-29', 2, 1, NULL, NULL),
-(2, 'Picasso', 'Je suis une description nulle d\'article', 'Musee', 100, 0, 1, 0, '2020-04-29', 5, 1, NULL, NULL),
-(3, 'Anneau', 'Je suis une description nulle d\'article', 'VIP', 200, 0, 0, 1, '2020-04-21', NULL, 1, NULL, NULL),
-(4, 'La nuit étoilée', 'Je suis une description nulle d\'article', 'Musee', 200, 0, 1, 1, '2020-04-21', NULL, 1, NULL, NULL);
+(1, 'Pièce de 2€ commémorative 2017', 'Cette pièce a été crée pour le 25ème anniversaire du ruban rose qui lutte contre le cancer du sein.', 'Ferraille', 50, 1, 0, 0, '2020-04-29', 2, 1, NULL, NULL),
+(2, 'Jeune fille devant un miroir ,Pablo Picasso', 'Ce tableau sur toile de Pablo Picasso est l\'un de ses plus grand chefs-d\'œuvre. Cette peinture a suscité diverses interprétations, c\'est ce qui fait son charme.', 'Musee', 100, 0, 1, 0, '2020-04-29', 5, 1, NULL, NULL),
+(3, 'Anneau Ancestral', 'Cet anneau est tout droit sorti d\'un film de science fiction.', 'VIP', 200, 0, 0, 1, '2020-04-21', NULL, 1, NULL, NULL),
+(4, 'La nuit étoilée, Van Gogh', 'Le tableau est une huile sur toile, il représente ce que Van Gogh pouvait voir de sa chambre d’asile.', 'Musee', 1200, 0, 1, 1, '2020-04-21', NULL, 1, NULL, NULL),
+(5, 'kylix, Vase Grec', 'Vase datant de 530 av J.-C fait en Céramique.', 'VIP', 350, 1, 0, 0, '2020-04-29', NULL, 5, NULL, NULL),
+(6, 'Aiguière', 'Aiguière en émail peint. La panse de l’aiguière est ornée d’une frise en grisaille.', 'VIP', 800, 0, 0, 1, '2020-04-29', NULL, 4, NULL, NULL),
+(7, 'IKB, Yves Klein', 'Ce bleu plus que bleu représente un ciel d’été. Il est d’une profondeur parfaite.', 'Musée', 750, 1, 0, 0, '2020-04-29', NULL, 3, NULL, NULL),
+(8, 'St-Georges-Majeur au crépuscule, Claude Monet', 'Cette peinture a été faite pendant la visite de Claude Monet à Venise en automne 1908.', 'Musée', 900, 0, 0, 1, '2020-04-29', NULL, 2, NULL, NULL),
+(9, 'Bague Saphira', 'Bague composé d’un saphir ovale de 6.90g sertie de cristaux de diamants blanc.', 'Ferraille', 250, 0, 1, 0, '2020-04-29', NULL, 5, NULL, NULL),
+(10, 'Médaillon en or', 'Médaillon ancien rond datant du XVème siècle.', 'Ferraille', 50, 1, 0, 0, '2020-04-29', NULL, 4, NULL, NULL),
+(11, 'Dinar en or Umayyad', 'Lot de deux pièces d’or fabriqué à Damas en 723.', 'Ferraille', 550, 0, 0, 1, '2020-04-29', NULL, 4, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -266,14 +273,21 @@ CREATE TABLE IF NOT EXISTS `image` (
 --
 
 INSERT INTO `image` (`CheminImg`, `Nom`, `#IDArticle`, `#IDVendeur`) VALUES
-('../img/Articles/anneau.jpg', 'Anneau', 3, NULL),
-('../img/Articles/Picasso.jpg', 'Picasso', 2, NULL),
-('../img/Articles/piecerare.jpg', 'Pièce Rare', 1, NULL),
-('../img/Articles/tabVanGogh.jpg', 'La nuit étoilée', 4, NULL),
 ('../img/Vendeur/collectionneur.jpg', 'Collectionneur', NULL, 1),
 ('../img/Vendeur/collectionneurBack.jpg', 'Magasin vendeur collectionneur', NULL, 1),
+('https://cdn.catawiki.net/assets/marketing/uploads-files/43483-ef5f6a7ae6499c8f2dd234af43b10b3e2ad74eaa-story_inline_image.jpg', 'Dinar', 11, NULL),
 ('https://images.jeugeek.com/uploads/files/csgo-ranks.jpg', 'jeanvaljean', NULL, 2),
-('https://static-www.elastic.co/v3/assets/bltefdd0b53724fa2ce/blt73c524420c2ba62c/5ca6896ee2a0d75e33470a83/sql-search.jpg', 'jeanvaljean', NULL, 2);
+('https://medias.gazette-drouot.com/prod/medias/mediatheque/25336.jpg', 'La nuit étoilée', 4, NULL),
+('https://s3-eu-west-1.amazonaws.com/auctionmediaphotos/e/8/2/1500461407999070.jpg', 'Saphira', 9, NULL),
+('https://static-www.elastic.co/v3/assets/bltefdd0b53724fa2ce/blt73c524420c2ba62c/5ca6896ee2a0d75e33470a83/sql-search.jpg', 'jeanvaljean', NULL, 2),
+('https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Claude_Monet%2C_Saint-Georges_majeur_au_cr%C3%A9puscule.jpg/1024px-Claude_Monet%2C_Saint-Georges_majeur_au_cr%C3%A9puscule.jpg', 'Monet Crépuscule', 8, NULL),
+('https://www.bijouxbaume.com/upload/image/pendentif-medaillon-ancien-en-or-cisele-p-image-67787-grande.jpg', 'Médaillon', 10, NULL),
+('https://www.centrepompidou.fr/media/picture/b7/40/b74000b2a938a9ec769f80beca98cb77/thumb_large.jpg', 'Klein Blue', 7, NULL),
+('https://www.cinemalechambord.fr/evenement/MARATHON_SEIGNEUR_DES_ANNEAUX.jpg', 'Anneau', 3, NULL),
+('https://www.louvre.fr/sites/default/files/imagecache/940x768/medias/medias_images/images/louvre-aiguiere-passage-mer-rouge.jpg', 'Aiguière', 6, NULL),
+('https://www.pauloeuvreart.com/couch/uploads/image/paintings/picasso/picasso-girl-before-a-mirror.jpg', 'Picasso', 2, NULL),
+('https://www.smh-stockage.com/wp-content/uploads/2017/09/vase.jpg', 'Vase Grec', 5, NULL),
+('https://www.yvert.com/I-Grande-137237-2-euro-commemorative-2017-france-lutte-cancer-sein.net.jpg', 'Pièce Rare', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -317,7 +331,7 @@ CREATE TABLE IF NOT EXISTS `vendeur` (
   `Mail` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
   PRIMARY KEY (`IDVendeur`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `vendeur`
@@ -325,7 +339,10 @@ CREATE TABLE IF NOT EXISTS `vendeur` (
 
 INSERT INTO `vendeur` (`IDVendeur`, `Pseudo`, `Mail`, `Password`) VALUES
 (1, 'Collectionneur', 'collectionneur@edu.ece.fr', '1234'),
-(2, 'jeanvaljean', 'jeanvaljean@edu.ece.fr', '1234');
+(2, 'jeanvaljean', 'jeanvaljean@edu.ece.fr', '1234'),
+(3, 'pierrequiroule', 'pierrequiroule@edu.ece.fr', '1234'),
+(4, 'alaindain', 'alaindain@edu.ece.fr', '1234'),
+(5, 'thomasdebateau', 'thomasdebateau@edu.ece.fr', '1234');
 
 --
 -- Contraintes pour les tables déchargées
