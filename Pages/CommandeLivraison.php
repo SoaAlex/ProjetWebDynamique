d6473b74d8e2f84edb836a7a7f7d8d6be2437965
SoaAlex <l.soaresalex@gmail.com>
2020-04-20 19:25:18 +0200

    JavaScript vers Jquery pour CommandeLivraison

--- Pages/CommandeLivraison.php
+++ Pages/CommandeLivraison.php
@@ -28,7 +28,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
-        <script type="text/javascript">
+        <!--<script type="text/javascript">
             function relais(){
                 document.getElementById('affichageRelais1').style.display ='none';
                 document.getElementById('affichageRelais2').style.display ='block';
@@ -40,6 +40,33 @@
                 document.getElementById('affichageRelais2').style.display ='none';
                 document.getElementById('affichageRelais3').style.display ='none';
             }
+        </script>-->
+
+        <!-- Du jQuerry gratuit -->
+        <script type="text/javascript">
+            $(document).ready(function(){
+                //Par défaut
+                $('#affichageRelais1').css('display', 'block');
+                $('#affichageRelais2').css('display', 'none');
+                $('#affichageRelais3').css('display', 'none');
+
+                //Si clique sur Express, on affiche pas les PR
+                $('#Express').click(function(){
+                    $('#affichageRelais1').css('display', 'block');
+                    $('#affichageRelais2').css('display', 'none');
+                    $('#affichageRelais3').css('display', 'none');
+                });
+                $('#Standard').click(function(){//Sur Standard, pareil
+                    $('#affichageRelais1').css('display', 'block');
+                    $('#affichageRelais2').css('display', 'none');
+                    $('#affichageRelais3').css('display', 'none');
+                });
+                $('#PR').click(function(){//Sur Point Relais, on affiche
+                    $('#affichageRelais1').css('display', 'none');
+                    $('#affichageRelais2').css('display', 'block');
+                    $('#affichageRelais3').css('display', 'block');
+                });
+            });
         </script>
     </head>
 
@@ -99,15 +126,15 @@
                    
                     <div class="form-check form-check-inline" style="margin-top: 20px">
                    
-                          <input class="form-check-input" type="radio" name="Livraison" class="exampleRadios" value=10 onclick="Prelais()" checked>
+                          <input class="form-check-input" type="radio" name="Livraison" id="Express" class="exampleRadios" value=10 onclick="Prelais()" checked>
                           <label class="form-check-label" for="exampleRadios1" style="width: 200px;  margin-right: 30px;">
                           Express: 10€
                           </label>
-                          <input class="form-check-input" type="radio" name="Livraison" class="exampleRadios" value=5 onclick="Prelais()">
+                          <input class="form-check-input" type="radio" name="Livraison" id="Standard" class="exampleRadios" value=5 onclick="Prelais()">
                           <label class="form-check-label" for="exampleRadios1" style="width: 200px;  margin-right: 30px;">
                           Standard: 5€
                           </label>
-                          <input class="form-check-input" type="radio" name="Livraison" class="exampleRadios" value=3 onclick="relais()">
+                          <input class="form-check-input" type="radio" name="Livraison" id="PR" class="exampleRadios" value=3 onclick="relais()">
                           <label class="form-check-label" for="exampleRadios1" style="width: 130px;  margin-right: 30px;">
                           Point relais: 3€
                           </label>
